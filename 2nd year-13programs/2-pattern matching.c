898c8ff7542f5466adcb13237afac223098005fc
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-07-19 22:47:33 +0530

    Update 2-pattern matching.c

--- 2nd year-13programs/2-pattern matching.c
+++ 2nd year-13programs/2-pattern matching.c
@@ -7,13 +7,13 @@ void main()
     int i, f = 0;
     printf("\nenter a string:");
     gets(s);
-    printf("\nenter a character to find its position:");
+    printf("\nenter the pattern to find:");
     scanf("%c", &c);
     for (i = strlen(s); i >= 0; i--)
     {
         if (s[i] == c)
         {
-            printf("character position:%d \n", i+1);
+            printf("pattern found at:%d \n", i+1);
             f = 1;
         }
     }
@@ -21,29 +21,3 @@ void main()
         printf("Position not found !");
     getch();
 }
-/* ABOVE GIVEN IS THE SIMPLEST PROGRAM TO SEARCH A CHARACTER. To search a string use the below code
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char s[20], p[20];
-    int a, b, i, j;
-    printf("Enter the string : ");
-    gets(s);
-    printf("Enter the pattern to find : ");
-    gets(p);
-
-    a = strlen(p);
-    b = strlen(s);
-
-    for (i = 0; i <= b - a; i++)
-    {
-        for (j = 0; j < a; j++)
-            if (s[i + j] != p[j])
-                break;
-        if (j == a)
-            printf("Pattern found position %d \n", i + 1);
-    }
-    return 0;
-}
-*/
