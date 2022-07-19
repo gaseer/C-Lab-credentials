72e54f5dcec84e524bf9a2a2294cc7905245331f
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-07-19 22:50:53 +0530

    Update 2-pattern matching.c

--- 2nd year-13programs/2-pattern matching.c
+++ 2nd year-13programs/2-pattern matching.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
+int main()
+{
+    char s[20], p[20];
+    int a, b, i, j;
+    printf("Enter the string : ");
+    gets(s);
+    printf("Enter the pattern to find : ");
+    gets(p);
+
+    a = strlen(p);
+    b = strlen(s);
+
+    for (i = 0; i <= b - a; i++)
+    {
+        for (j = 0; j < a; j++)
+            if (s[i + j] != p[j])
+                break;
+        if (j == a)
+            printf("Pattern found position %d \n", i + 1);
+    }
+    return 0;
+}
+
+
+/* This is the simplest program but it will only search charachters NOT PATTERN 
+#include <stdio.h>
+#include <string.h>
 void main()
 {
     char s[100], c;
@@ -21,3 +47,4 @@ void main()
         printf("Position not found !");
     getch();
 }
+*/
