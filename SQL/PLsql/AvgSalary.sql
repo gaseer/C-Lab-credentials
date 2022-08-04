e280d796428955228abf2216676b4bd4a44e0fd7
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-08-04 18:35:56 +0530

    Update AvgSalary.sql

--- SQL/PLsql/AvgSalary.sql
+++ SQL/PLsql/AvgSalary.sql
@@ -4,3 +4,12 @@ insert into emp_sal values(02,'Manju', 'sales', 4000);
 insert into emp_sal values(03,'sanju', 'marketing', 2000);
 select*from emp_sal;
 
+create function avgsal(deptt varchar(10))returns numeric(10,2) as
+'declare
+avgsal numeric(10,2);
+begin
+avgsal=(select avg(salary) from emp_sal group by edept having edept= deptt);
+return avgsal;
+end;'language plpgsql;
+
+select avgsal('sales');
