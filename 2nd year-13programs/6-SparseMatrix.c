cc4e86a9b9752f6475b39ead0f61f81a6a8eeb7f
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-08-04 19:32:00 +0530

    Update 6-SparseMatrix.c

--- 2nd year-13programs/6-SparseMatrix.c
+++ 2nd year-13programs/6-SparseMatrix.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 int main()
 {
-    int S[10][10], m, n, i, j, k = 0, size = 0;
-    printf("Enter no:of rows and cols for matrix :");
-    scanf("%d %d", &m, &n);
-    for (i = 0; i < m; i++)
+    int a[10][10], i, j, r, c, k = 0;
+    printf("Enter the rows and column of the sparce matrix:");
+    scanf("%d%d", &r, &c);
+    for (i = 0; i < r; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < c; j++)
         {
-            printf("Enter element in a%d%d : ", i + 1, j + 1);
-            scanf("%d", &S[i][j]);
+            printf("Enter values of a%d%d :", i + 1, j + 1);
+            scanf("%d", &a[i][j]);
         }
     }
-    printf("The matrix is \n");
-    for (i = 0; i < m; i++)
+    printf("the matrix is :\n");
+    for (i = 0; i < r; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < c; j++)
         {
-            printf("%d\t", S[i][j]);
-            if (S[i][j] != 0)
-                size++;
+            printf("%d\t", a[i][j]);
         }
         printf("\n");
     }
-    int M[3][size];
-    for (i = 0; i < m; i++)
-        for (j = 0; j < n; j++)
-            if (S[i][j] != 0)
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            if (a[i][j] == 0)
+                k = k + 1;
+    if (k <= (r * c) / 2)
+    {
+        printf("error! it is a not a sparce matrix ");
+    }
+    else
+    {
+        printf("the sparce matrix has %d non zero values\n", (r * c) - k);
+        printf("triplte reprasentation of a sparce matrix is \n");
+        printf("row|\t column|\t values|\n");
+        printf("%d\t%d\t%d\n", r, c, (r * c) - k);
+        for (i = 0; i < r; i++)
+            for (j = 0; j < c; j++)
             {
-                M[0][k] = i;
-                M[1][k] = j;
-                M[2][k] = S[i][j];
-                k++;
+                if (a[i][j] != 0)
+                    printf("%d\t%d\t%d\n", i, j, a[i][j]);
             }
-    printf("Triplet  matrix is \nROW | COLUMN | VALUE\n");
-    for (j = 0; j < size; j++)
-    {
-        for (i = 0; i < 3; i++)
-        {
-            printf("%d\t\t", M[i][j]);
-        }
-        printf("\n");
     }
     return 0;
 }
