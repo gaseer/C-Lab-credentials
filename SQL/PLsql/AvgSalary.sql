4f21a06da08a79260b099fd4408ed49876a10c52
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-08-04 17:54:20 +0530

    Update AvgSalary.sql

--- SQL/PLsql/AvgSalary.sql
+++ SQL/PLsql/AvgSalary.sql
@@ -1,1 +1,6 @@
-create table emp_sal(empno int,ename varchar(10),edept varchar(10),salary numeric);                                             
+create table emp_sal(empno int,ename varchar(10),edept varchar(10),salary numeric);      
+insert into emp_sal values(01,'raju', 'sales', 2000);
+insert into emp_sal values(02,'Manju', 'sales', 4000);
+insert into emp_sal values(03,'sanju', 'marketing', 2000);
+select*from emp_sal;
+
